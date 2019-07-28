b72dc8ef2394371db4075add4741923a4d6e5241
Campeanu <campeanu.it@yahoo.com>
2019-07-28 18:36:46 +0300

    Link json library and add Sandbox project

---
+++ Box3D/include/Box3D/Application.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
---
+++ Sandbox/src/Sandbox.cpp
@@ -0,0 +1,9 @@
+#include <Box3D.hpp>
+
+int main()
+{
+
+    std::cout << "Hello world" << std::endl;
+
+    system("PAUSE");
+}
